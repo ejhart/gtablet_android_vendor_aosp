e0dceabf2916d30f8104aab0b2fffc1d28041426
ejhart <ejhart5@alienone>
2011-02-24 23:13:28 -0800

    fixed

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 add_lunch_combo aosp_harmony-eng
 add_lunch_combo aosp_sholes-eng
 PATH=$PATH:$PWD/vendor/aosp/tools ; export PATH
 
-
-=======
-3243245 234${KL:"$K%# K$#":%K$#%TK$:T $WEk:T 
->>>>>>> upstream/gingerbread
-=======
-3243245 234${KL:"$K%# K$#":%K$#%TK$:T $WEk:T 
->>>>>>> 39cc7a2ba3ef5cf9de7d70a6de562bfa1bc366ca
