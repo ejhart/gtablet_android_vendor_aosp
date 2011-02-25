ca9b7ea2c055ba8987d22dc7a56e61b65b19fa6c
ejhart <ejhart5@alienone>
2011-02-24 23:03:47 -0800

    Merge commit '39cc7a2ba3ef5cf9de7d70a6de562bfa1bc366ca' into gingerbread
    Conflicts:
    	vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 add_lunch_combo aosp_harmony-eng
 add_lunch_combo aosp_sholes-eng
@@ -8,3 +9,6 @@ PATH=$PATH:$PWD/vendor/aosp/tools ; export PATH
 =======
 3243245 234${KL:"$K%# K$#":%K$#%TK$:T $WEk:T 
 >>>>>>> upstream/gingerbread
+=======
+3243245 234${KL:"$K%# K$#":%K$#%TK$:T $WEk:T 
+>>>>>>> 39cc7a2ba3ef5cf9de7d70a6de562bfa1bc366ca
