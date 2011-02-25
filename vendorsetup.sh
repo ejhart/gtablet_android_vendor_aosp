7c506505966112a1da38fb31ea9d4839fbc5e639
ejhart <ejhart5@alienone>
2011-02-24 22:43:00 -0800

    blah

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
+
 add_lunch_combo aosp_harmony-eng
 add_lunch_combo aosp_sholes-eng
 PATH=$PATH:$PWD/vendor/aosp/tools ; export PATH
 
-=======
-sassssssssssssss
 
-#this is a test this is only a test if this were not a test you would be dead
->>>>>>> upstream/master
